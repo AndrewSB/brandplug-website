ace4b072747bb0f28a4a77e4c3c6bb2ff4085e8e
Andrew Breckenridge <asbreckenridge@me.com>
2015-07-22 00:54:47 -0700

    add parse

--- js/urip.js
+++ js/urip.js
@@ -388,56 +388,6 @@ $(document).ready(function(){
         }
     });
 
-    /* ==================================
-	   Hero Form Validation
-	=====================================*/
-	$('#hero-submit').click(function(e){
-
-        // Stop form submission & check the validation
-        e.preventDefault();
-
-        // Variable declaration
-        var error = false;
-        var fname = $('#hero-fname').val();
-        var email = $('#hero-email').val();
-        var username = $('#hero-username').val();
-
-     	// Form field validation
-        if(fname.length == 0){
-            var error = true;
-            $('#hero-fname').parent('div').addClass('field-error');
-        }else{
-            $('#hero-fname').parent('div').removeClass('field-error');
-        }
-        if(email.length == 0 || email.indexOf('@') == '-1'){
-            var error = true;
-            $('#hero-email').parent('div').addClass('field-error');
-        }else{
-            $('#hero-email').parent('div').removeClass('field-error');
-        }
-        if(username.length == 0){
-            var error = true;
-            $('#hero-username').parent('div').addClass('field-error');
-        }else{
-            $('#hero-username').parent('div').removeClass('field-error');
-        }
-
-        if(error == true){
-        	$('#hero-error-notification').addClass('show-up');
-        }else{
-           $('#hero-error-notification').removeClass('show-up');
-        }
-
-        if(error == false){
-            $.post("hero-form.php", $("#register-form").serialize(),function(result){
-                if(result == 'sent'){
-                    $('#hero-success-notification').addClass('show-up');
-                    $('#hero-submit').addClass('disabled');
-                }
-            });
-        }
-    });
-
 
 	// Function to close the Notification
     $('a.notification-close').click(function(){
@@ -468,6 +418,3 @@ $(document).ready(function(){
 	}
 
 });
-
-
-
